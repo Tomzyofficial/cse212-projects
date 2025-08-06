f58e55288ef440b6438c87ca7ee1b7396078f2b5
Tomzyofficial <tomzyofficial54@gmail.com>
2025-08-06 23:55:35 +0100

    W06 Code: trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,45 +1,63 @@
 public class Node
 {
-    public int Data { get; set; }
-    public Node? Right { get; private set; }
-    public Node? Left { get; private set; }
+  public int Data { get; set; }
+  public Node? Right { get; private set; }
+  public Node? Left { get; private set; }
 
-    public Node(int data)
+  public Node(int data)
+  {
+    this.Data = data;
+  }
+
+  public void Insert(int value)
+  {
+    // TODO Start Problem 1
+    if (value == Data) return;
+
+    if (value < Data)
     {
-        this.Data = data;
+      // Insert to the left
+      if (Left is null)
+        Left = new Node(value);
+      else
+        Left.Insert(value);
+    }
+    else
+    {
+      // Insert to the right
+      if (Right is null)
+        Right = new Node(value);
+      else
+        Right.Insert(value);
+    }
+  }
+
+  public bool Contains(int value)
+  {
+    // TODO Start Problem 2
+    if (value == Data)
+    {
+      return true;
     }
 
-    public void Insert(int value)
+    if (value < Data)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+      return Left != null && Left.Contains(value);
     }
 
-    public bool Contains(int value)
+    if (value > Data)
     {
-        // TODO Start Problem 2
-        return false;
+      return Right != null && Right.Contains(value);
     }
+    return false;
+  }
 
-    public int GetHeight()
-    {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
-    }
+  public int GetHeight()
+  {
+    // TODO Start Problem 4
+    int leftHeight = Left != null ? Left.GetHeight() : 0;
+    int rightHeight = Right != null ? Right.GetHeight() : 0;
+
+    return 1 + Math.Max(leftHeight, rightHeight); // Replace this line with the correct return statement(s)
+  }
 }
